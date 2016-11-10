c6222b1bc9a7a6f6d2c9ab94d351587f609b5d16
Jari Avikainen <jari.avikainen@gmail.com>
2016-11-10 15:47:22 +0200

    Add handling for bets + methods clearHand() and getChoice()

--- Blackjack/src/main/java/blackjack/logic/Player.java
+++ Blackjack/src/main/java/blackjack/logic/Player.java
@@ -5,16 +5,27 @@ package blackjack.logic;
  * @author Jari Avikainen
  */
 public class Player {
-    
+
     private final String name;
-    
+
     private int money;
 
+    private int currentBet;
+
     private Hand hand;
-    
+
     public Player(String name, int money) {
         this.name = name;
         this.money = money;
+        this.currentBet = 0;
+    }
+
+    public int getCurrentBet() {
+        return this.currentBet;
+    }
+
+    public void clearBet() {
+        this.currentBet = 0;
     }
 
     public String getName() {
@@ -28,13 +39,26 @@ public class Player {
     public void addMoney(int amount) {
         this.money += amount;
     }
-    
+
     public Hand getHand() {
         return hand;
+    }
+
+    public void clearHand() {
+        this.hand = new Hand();
     }
 
     public void addCard(Card card) {
         this.hand.add(card);
     }
-    
+
+    public void setBet() {
+        int newBet = 5; // Get input from player
+        this.money += currentBet - newBet;
+        currentBet = newBet;
+    }
+
+    public Choice getChoice() {
+        return Choice.STAND;
+    }
 }
