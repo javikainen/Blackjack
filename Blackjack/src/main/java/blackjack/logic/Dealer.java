3beb14a3c5605eee5681bd948cbc210c3249960c
Jari Avikainen <jari.avikainen@gmail.com>
2016-11-10 15:48:25 +0200

    Add basic gameflow

--- Blackjack/src/main/java/blackjack/logic/Dealer.java
+++ Blackjack/src/main/java/blackjack/logic/Dealer.java
@@ -7,13 +7,57 @@ import java.util.ArrayList;
  * @author Jari Avikainen
  */
 public class Dealer {
-    
+
     Hand hand;
     Shoe shoe;
     ArrayList<Player> players;
-    
+
     public Hand getHand() {
         return this.hand;
     }
+
+    public void getBets() {
+        for (Player player : players) {
+            player.setBet();
+        }
+    }
+
+    public void deal() {
+        for (int i = 0; i < 2; i++) {
+            for (Player player : players) {
+                if (!shoe.hasCards()) {
+                    shoe.shuffle();
+                }
+                player.addCard(shoe.getCard());
+            }
+            this.hand.add(shoe.getCard());
+        }
+    }
     
+    public void resolveRound() {
+        for (Player player : players) {
+            resolveHand(player);
+        }
+        this.hand = new Hand();
+    }
+
+    public void resolveHand(Player player) {
+        Choice playerChoice = player.getChoice();
+        boolean playerWin = true;
+        if (playerWin) {
+            player.addMoney(player.getCurrentBet());
+        } else {
+            player.clearBet();
+        }
+        player.clearHand();
+    }
+    
+    public void play() {
+        while(true) {
+            getBets();
+            deal();
+            resolveRound();
+        }
+    }
+
 }
