249dd0008de06cadbc1775a75ec281dff65e588e
Jari Avikainen <jari.avikainen@gmail.com>
2016-11-17 16:28:02 +0200

    Add functionality from class Dealer. Start work on resolving rounds.

--- Blackjack/src/main/java/blackjack/logic/BlackjackLogic.java
+++ Blackjack/src/main/java/blackjack/logic/BlackjackLogic.java
@@ -1,6 +1,5 @@
 package blackjack.logic;
 
-import blackjack.gui.BlackjackUI;
 import java.util.ArrayList;
 
 /**
@@ -8,24 +7,64 @@ import java.util.ArrayList;
  * @author Jari Avikainen
  */
 public class BlackjackLogic {
-    
-    BlackjackUI ui;
+
     ArrayList<Player> players;
     Dealer dealer;
-    
-    public BlackjackLogic(BlackjackUI ui) {
-        this.ui = ui;
+    int nextPlayer;
+    Hand dealerHand;
+    Shoe shoe;
+
+    public BlackjackLogic() {
+        players = new ArrayList<>();
+        players.add(new Player("Player 1", 1000));
     }
-    
+
     public Hand getDealerHand() {
-        return dealer.getHand();
+        return dealerHand;
     }
-    
+
     public Hand getPlayerHand(Player player) {
         return player.getHand();
     }
+
+    public void setBets(int userBet) {
+        for (Player player : players) {
+            player.setBet(userBet);
+        }
+    }
+
+    public void deal() {
+        for (int i = 0; i < 2; i++) {
+            for (Player player : players) {
+                if (!shoe.hasCards()) {
+                    shoe.shuffle();
+                }
+                player.addCard(shoe.getCard());
+            }
+            dealerHand.add(shoe.getCard());
+        }
+        nextPlayer = 0;
+    }
     
+    public Card dealCard() {
+        if (!shoe.hasCards()) {
+            shoe.shuffle();
+        }
+        return shoe.getCard();
+    }
+
+    public Player resolvePlayer() {
+        Player player = players.get(nextPlayer);
+        if (player.isAI()) {
+            // do something
+            return player;
+        } else {
+            // do something else
+            return player;
+        }
+    }
     
-    
-    
+    public boolean bust(Player player) {
+        return player.getHand().getValue() > 21;
+    }
 }
