f9f9f822564a33cf0cf08aee3d7a790d54d26b93
Jari Avikainen <jari.avikainen@gmail.com>
2016-11-24 01:04:25 +0200

    Override toString()

--- Blackjack/src/main/java/blackjack/logic/Suit.java
+++ Blackjack/src/main/java/blackjack/logic/Suit.java
@@ -5,5 +5,21 @@ package blackjack.logic;
  * @author Jari Avikainen
  */
 public enum Suit {
-    DIAMONDS, HEARTS, CLUBS, SPADES
+
+    DIAMONDS, HEARTS, CLUBS, SPADES;
+
+    @Override
+    public String toString() {
+        switch (this) {
+            case DIAMONDS:
+                return "D";
+            case HEARTS:
+                return "H";
+            case CLUBS:
+                return "C";
+            case SPADES:
+                return "S";
+        }
+        return "?";
+    }
 }
