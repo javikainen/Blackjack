c982368223ca0bd3f40c86754bd5a003c50f1bd1
Jari Avikainen <jari.avikainen@gmail.com>
2016-11-24 15:55:26 +0200

    Add tests for hashCode() and equals()

--- Blackjack/src/test/java/blackjack/logic/CardTest.java
+++ Blackjack/src/test/java/blackjack/logic/CardTest.java
@@ -1,5 +1,6 @@
 package blackjack.logic;
 
+import java.util.Objects;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -28,6 +29,23 @@ public class CardTest {
     @Test
     public void testGetRank() {
         assertEquals(1, card.getRank());
+    }
+    
+    @Test
+    public void testEquals() {
+        assertTrue(card.equals(new Card(Suit.DIAMONDS, 1)));
+        assertFalse(card.equals(new Card(Suit.DIAMONDS, 6)));
+        assertFalse(card.equals(new Card(Suit.CLUBS, 1)));
+        assertFalse(card.equals(null));
+        assertFalse(card.equals(new Shoe(1)));
+    }
+    
+    @Test
+    public void testHashCode() {
+        int correctHash = 3;
+        correctHash = 59 * correctHash + Objects.hashCode(card.getSuit());
+        correctHash = 59 * correctHash + card.getRank();
+        assertEquals(correctHash, card.hashCode());
     }
     
 }
