7133c3fdbd395f006358ce7e777e0645f7cfd873
Jari Avikainen <jari.avikainen@gmail.com>
2016-11-24 16:18:46 +0200

    Change setBet() to use superclass methods

--- Blackjack/src/main/java/blackjack/logic/HumanPlayer.java
+++ Blackjack/src/main/java/blackjack/logic/HumanPlayer.java
@@ -8,15 +8,20 @@ public class HumanPlayer extends Player {
 
     public HumanPlayer(String name, int id, int money) {
         super(name, id, money);
-        super.isAI = false;
+        isAI = false;
     }
 
     @Override
     public void setBet(int userBet) {
         if (userBet > 0) {
             int newBet = userBet; // Use the bet chosen by the user
-            super.money += super.currentBet - newBet;
-            super.currentBet = newBet;
+            int change = getCurrentBet() - newBet;
+            if (change >= 0) {
+                addMoney(change);
+            } else {
+                takeMoney(0 - change);
+            }
+            setCurrentBet(newBet);
         }
     }
 
