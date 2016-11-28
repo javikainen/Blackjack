f51b4068dad04ee2ce95059e2f361deb9cb695a7
Jari Avikainen <jari.avikainen@gmail.com>
2016-11-28 13:23:17 +0200

    Add missing tests

--- Blackjack/src/test/java/blackjack/logic/BlackjackLogicTest.java
+++ Blackjack/src/test/java/blackjack/logic/BlackjackLogicTest.java
@@ -21,15 +21,15 @@ public class BlackjackLogicTest {
         gameLogic = new BlackjackLogic(2, 1, 1000);
     }
 
-    @Test(expected=IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void testConstructorRefusesInvalidArguments() {
-            gameLogic = new BlackjackLogic(-1, 1, 1000);
-            gameLogic = new BlackjackLogic(3, 1, 1000);
-            gameLogic = new BlackjackLogic(2, 0, 1000);
-            gameLogic = new BlackjackLogic(2, 1, 0);
-        assertTrue(true);
+        gameLogic = new BlackjackLogic(-1, 1, 1000);
+        gameLogic = new BlackjackLogic(3, 1, 1000);
+        gameLogic = new BlackjackLogic(2, 0, 1000);
+        gameLogic = new BlackjackLogic(2, 1, 0);
+//        assertTrue(true);
     }
-    
+
     @Test
     public void testNewBlackjackLogicContainsHumanPlayer() {
         List<Player> players = gameLogic.getPlayers();
@@ -44,14 +44,17 @@ public class BlackjackLogicTest {
 
     @Test
     public void testNewBlackJackLogicHasCorrentNumberOfAIPlayers() {
-        List<Player> players = gameLogic.getPlayers();
-        int numAIs = 0;
-        for (Player player : players) {
-            if (player.isAI() && !player.isDealer()) {
-                numAIs++;
+        for (int i = 0; i < 3; i++) {
+            gameLogic = new BlackjackLogic(i, 0, 1000);
+            List<Player> players = gameLogic.getPlayers();
+            int numAIs = 0;
+            for (Player player : players) {
+                if (player.isAI() && !player.isDealer()) {
+                    numAIs++;
+                }
             }
+            assertEquals(i, numAIs);
         }
-        assertEquals(2, numAIs);
     }
 
     @Test
@@ -63,7 +66,7 @@ public class BlackjackLogicTest {
         assertEquals(null, gameLogic.getNextPlayer());
         assertEquals(gameLogic.getPlayers().get(0), gameLogic.getNextPlayer());
     }
-    
+
     @Test
     public void AIPlayersHaveCorrectNamesAndIDs() {
         for (int i = 0; i < 2; i++) {
@@ -82,7 +85,7 @@ public class BlackjackLogicTest {
         assertEquals("AI 1", player.getName());
         assertEquals(2, player.getID());
     }
-    
+
     @Test
     public void testGetPlayerHand() {
         Player player = gameLogic.getNextPlayer();
@@ -96,7 +99,7 @@ public class BlackjackLogicTest {
         assertTrue(first.getSuit() == Suit.CLUBS && first.getRank() == 1);
         assertTrue(second.getSuit() == Suit.CLUBS && second.getRank() == 11);
     }
-    
+
     @Test
     public void testSetBets() {
         gameLogic.adjustBets(20);
@@ -112,26 +115,173 @@ public class BlackjackLogicTest {
             }
         }
     }
-    
+
     @Test
-    public void testPayWinningsWhenDealerHasBlackjack() {
+    public void testPayWinningsClearsHands() {
         List<Player> players = gameLogic.getPlayers();
-        Hand dH = gameLogic.getDealerHand();
-        for (Player player : players) {
-            player.setCurrentBet(50);
-        }
-        dH.add(new Card(Suit.CLUBS, 11));
-        dH.add(new Card(Suit.CLUBS, 1));
         Player p0 = players.get(0);
         Player p1 = players.get(1);
         Player p2 = players.get(2);
+        Player dealer = players.get(3);
+        dealer.addCard(new Card(Suit.CLUBS, 1));
+        dealer.addCard(new Card(Suit.CLUBS, 11));
         p0.addCard(new Card(Suit.DIAMONDS, 11));
         p0.addCard(new Card(Suit.DIAMONDS, 1));
-        p1.addCard(new Card(Suit.DIAMONDS, 11));
-        p1.addCard(new Card(Suit.DIAMONDS, 1));
-        p2.addCard(new Card(Suit.DIAMONDS, 11));
-        p2.addCard(new Card(Suit.DIAMONDS, 1));
+        p1.addCard(new Card(Suit.HEARTS, 11));
+        p1.addCard(new Card(Suit.HEARTS, 5));
+        p1.addCard(new Card(Suit.HEARTS, 6));
+        p2.addCard(new Card(Suit.SPADES, 11));
+        p2.addCard(new Card(Suit.SPADES, 5));
+        p2.addCard(new Card(Suit.SPADES, 7));
+        gameLogic.payWinnings();
+        for (Player player : players) {
+            assertTrue(player.getHand().getContents().isEmpty());
+        }
+    }
 
+    @Test
+    public void testPayWinningsWhenDealerHasBlackjack() {
+        List<Player> players = gameLogic.getPlayers();
+        for (Player player : players) {
+            player.setCurrentBet(50);
+        }
+        Player p0 = players.get(0);
+        Player p1 = players.get(1);
+        Player p2 = players.get(2);
+        Player dealer = players.get(3);
+        dealer.addCard(new Card(Suit.CLUBS, 1));
+        dealer.addCard(new Card(Suit.CLUBS, 11));
+        p0.addCard(new Card(Suit.DIAMONDS, 11));
+        p0.addCard(new Card(Suit.DIAMONDS, 1));
+        p1.addCard(new Card(Suit.HEARTS, 11));
+        p1.addCard(new Card(Suit.HEARTS, 5));
+        p1.addCard(new Card(Suit.HEARTS, 6));
+        p2.addCard(new Card(Suit.SPADES, 11));
+        p2.addCard(new Card(Suit.SPADES, 5));
+        p2.addCard(new Card(Suit.SPADES, 7));
+        gameLogic.payWinnings();
+        assertEquals(1000, p0.getMoney());
+        assertEquals(950, p1.getMoney());
+        assertEquals(950, p2.getMoney());
+    }
+
+    @Test
+    public void testPayWinningsWhenDealerHas21() {
+        List<Player> players = gameLogic.getPlayers();
+        for (Player player : players) {
+            player.setCurrentBet(50);
+        }
+        Player p0 = players.get(0);
+        Player p1 = players.get(1);
+        Player p2 = players.get(2);
+        Player dealer = players.get(3);
+        dealer.addCard(new Card(Suit.CLUBS, 4));
+        dealer.addCard(new Card(Suit.CLUBS, 7));
+        dealer.addCard(new Card(Suit.CLUBS, 11));
+        p0.addCard(new Card(Suit.DIAMONDS, 11));
+        p0.addCard(new Card(Suit.DIAMONDS, 1));
+        p1.addCard(new Card(Suit.HEARTS, 11));
+        p1.addCard(new Card(Suit.HEARTS, 5));
+        p1.addCard(new Card(Suit.HEARTS, 6));
+        p2.addCard(new Card(Suit.SPADES, 11));
+        p2.addCard(new Card(Suit.SPADES, 12));
+        gameLogic.payWinnings();
+        assertEquals(1075, p0.getMoney());
+        assertEquals(1000, p1.getMoney());
+        assertEquals(950, p2.getMoney());
+    }
+
+    @Test
+    public void testPayWinningsWhenDealerHas17() {
+        List<Player> players = gameLogic.getPlayers();
+        for (Player player : players) {
+            player.setCurrentBet(50);
+        }
+        Player p0 = players.get(0);
+        Player p1 = players.get(1);
+        Player p2 = players.get(2);
+        Player dealer = players.get(3);
+        dealer.addCard(new Card(Suit.CLUBS, 7));
+        dealer.addCard(new Card(Suit.CLUBS, 11));
+        p0.addCard(new Card(Suit.DIAMONDS, 11));
+        p0.addCard(new Card(Suit.DIAMONDS, 1));
+        p1.addCard(new Card(Suit.HEARTS, 11));
+        p1.addCard(new Card(Suit.HEARTS, 5));
+        p1.addCard(new Card(Suit.HEARTS, 3));
+        p2.addCard(new Card(Suit.SPADES, 11));
+        p2.addCard(new Card(Suit.SPADES, 7));
+        gameLogic.payWinnings();
+        assertEquals(1075, p0.getMoney());
+        assertEquals(1050, p1.getMoney());
+        assertEquals(1000, p2.getMoney());
+    }
+
+    @Test
+    public void testPayWinningsWhenDealerisBust() {
+        List<Player> players = gameLogic.getPlayers();
+        for (Player player : players) {
+            player.setCurrentBet(50);
+        }
+        Player p0 = players.get(0);
+        Player p1 = players.get(1);
+        Player p2 = players.get(2);
+        Player dealer = players.get(3);
+        dealer.addCard(new Card(Suit.CLUBS, 6));
+        dealer.addCard(new Card(Suit.CLUBS, 11));
+        dealer.addCard(new Card(Suit.CLUBS, 12));
+        p0.addCard(new Card(Suit.DIAMONDS, 11));
+        p0.addCard(new Card(Suit.DIAMONDS, 1));
+        p1.addCard(new Card(Suit.HEARTS, 2));
+        p2.addCard(new Card(Suit.SPADES, 11));
+        p2.addCard(new Card(Suit.SPADES, 7));
+        p2.addCard(new Card(Suit.SPADES, 12));
+        gameLogic.payWinnings();
+        assertEquals(1075, p0.getMoney());
+        assertEquals(1050, p1.getMoney());
+        assertEquals(950, p2.getMoney());
+    }
+
+    @Test
+    public void testDealCard() {
+        Player player = new HumanPlayer("Test", 1, 1000);
+        gameLogic.dealCard(player);
+        assertEquals(1, player.getHand().getContents().size());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testPlayAIHandRefusesHumanArgument() {
+        Player human = new HumanPlayer("Human", 1, 1000);
+        gameLogic.playAIHand(human);
+    }
+
+    @Test
+    public void testPlayAIHand() {
+        Player AI = new AIPlayer("AI", 1, 1000);
+        gameLogic.playAIHand(AI);
+        assertFalse(AI.getHand().getContents().isEmpty());
+    }
+
+    @Test
+    public void testGetDealerHand() {
+        List<Player> players = gameLogic.getPlayers();
+        assertEquals(players.get(3).getHand(), gameLogic.getDealerHand());
+    }
+
+    @Test
+    public void testAllPlayersHaveTwoCardsAfterDeal() {
+        List<Player> players = gameLogic.getPlayers();
+        gameLogic.deal();
+        for (Player player : players) {
+            assertEquals(2, player.getHand().getContents().size());
+        }
+    }
+
+    @Test
+    public void testFirstPlayerIsNextPlayerAfterDeal() {
+        List<Player> players = gameLogic.getPlayers();
+        gameLogic.getNextPlayer();
+        gameLogic.deal();
+        assertEquals(players.get(0), gameLogic.getNextPlayer());
     }
 
 }
